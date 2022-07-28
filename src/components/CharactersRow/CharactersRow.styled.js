972f3bc953632de5b3e4895a83b25e41bd4d0dc6
Aveiro Ortiz <aomservices@gmail.com>
2022-07-27 20:13:39 -0500

    Adding CharactersRow component

---
+++ src/components/CharactersRow/CharactersRow.styled.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export const StyledGrid = styled.div`
+    align-items: center;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+`;
+
+export const StyledRow = styled.div`
+    border-bottom: 1px solid #ccc;
+    padding: 40px 0;
+`;
+
+export const StyledTitleSection = styled.div`
+    color: #EB1D36;
+    font-size: 28px;
+    margin-bottom: 12px;
+    text-align: left;
+`;
