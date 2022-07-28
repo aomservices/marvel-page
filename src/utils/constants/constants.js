f29507790c6a32171f021981021b74df94e21d63
Aveiro Ortiz <aomservices@gmail.com>
2022-07-27 19:34:14 -0500

    Reordering constants folder

---
+++ src/utils/constants/constants.js
@@ -0,0 +1,13 @@
+export const API_BASE = 'https://gateway.marvel.com/v1/public'
+
+// aomservices@gmail.com
+export const PUBLIC_KEY = 'b2a79383223f2f9da052dc17c27b5406';
+export const PRIVATE_KEY = '6c03e1cc1966c0da2a167b1e5a3453955f246453';
+
+// theaom90@hotmail.com
+// export const PUBLIC_KEY = '7d5914c6280e175e1b5c85843120eee3';
+// export const PRIVATE_KEY = 'c40cd412945a72bd03449c4073374c25ebeb6c08';
+
+// GSM 
+// export const PUBLIC_KEY = '';
+// export const PRIVATE_KEY = '';
